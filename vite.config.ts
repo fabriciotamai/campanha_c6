d56f559849091ee3a41ce1ac05d59030118acce0
fabriciotamai <59937379+fabriciotamai@users.noreply.github.com>
2024-11-25 19:09:48 -0300

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Gera a build na pasta dist
+  },
+  server: {
+    open: true, // Abre o navegador automaticamente no ambiente local
+  },
+});
