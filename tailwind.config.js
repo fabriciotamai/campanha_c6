735817f42a5397fab98f0000ef9f2ff5ccea7bea
fabriciotamai <59937379+fabriciotamai@users.noreply.github.com>
2024-12-06 15:48:06 -0300

    (fix)vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -24,4 +24,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
